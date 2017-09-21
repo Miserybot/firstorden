d69a203e797ee1c760d910169fddc2df6c4fc70a
Leo <leomotovskikh@gmail.com>
2017-09-21 12:21:27 +0300

    Bosses shortcut?

--- core/functions/bosses.py
+++ core/functions/bosses.py
@@ -5,52 +5,29 @@ from core.utils import send_async, update_group
 
 
 @trigger_decorator
+def boss_common(bot: Bot, update: Update, level_from, level_to):
+    group = update_group(update.message.chat)
+    if len(group.squad) == 1:
+        members = []
+        for member in group.squad[0].members:
+            char = member.user.character
+            if level_from <= char.level <= level_to:
+                members.append('@' + member.user.username)
+        msg = ' '.join(members)
+        send_async(bot, chat_id=update.message.chat.id, text=msg)
+
+
 def boss_leader(bot: Bot, update: Update):
-    group = update_group(update.message.chat)
-    if len(group.squad) == 1:
-        members = []
-        for member in group.squad[0].members:
-            char = member.user.character
-            if 15 <= char.level <= 25:
-                members.append('@' + member.user.username)
-        msg = ' '.join(members)
-        send_async(bot, chat_id=update.message.chat.id, text=msg)
+    boss_common(bot, update, 15, 25)
 
 
-@trigger_decorator
 def boss_zhalo(bot: Bot, update: Update):
-    group = update_group(update.message.chat)
-    if len(group.squad) == 1:
-        members = []
-        for member in group.squad[0].members:
-            char = member.user.character
-            if 26 <= char.level <= 35:
-                members.append('@' + member.user.username)
-        msg = ' '.join(members)
-        send_async(bot, chat_id=update.message.chat.id, text=msg)
+    boss_common(bot, update, 26, 35)
 
 
-@trigger_decorator
 def boss_monoeye(bot: Bot, update: Update):
-    group = update_group(update.message.chat)
-    if len(group.squad) == 1:
-        members = []
-        for member in group.squad[0].members:
-            char = member.user.character
-            if 36 <= char.level <= 45:
-                members.append('@' + member.user.username)
-        msg = ' '.join(members)
-        send_async(bot, chat_id=update.message.chat.id, text=msg)
+    boss_common(bot, update, 36, 45)
 
 
-@trigger_decorator
 def boss_hydra(bot: Bot, update: Update):
-    group = update_group(update.message.chat)
-    if len(group.squad) == 1:
-        members = []
-        for member in group.squad[0].members:
-            char = member.user.character
-            if 46 <= char.level:
-                members.append('@' + member.user.username)
-        msg = ' '.join(members)
-        send_async(bot, chat_id=update.message.chat.id, text=msg)
+    boss_common(bot, update, 46, 100500)
