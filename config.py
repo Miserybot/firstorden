15866820efb595ce2c5a828b5065c7fbb72aa547
Pluck <35307675+Miserybot@users.noreply.github.com>
2018-01-12 00:20:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Содержимое этого файла необходимо скопировать в config.py и изменить под себя
 
 TOKEN = '542255798:AAGLKRHK4ehyMGBE6Oz2HED9Dd0TU3wV91w' # Токен телеграмбота (получается у @BotFather)
-DB = 'mysql://be7e037166f6fd:9120268b@eu-cdbr-west-01.cleardb.com/heroku_2628e0f251e73f5?charset=utf8mb4?reconnect=true' # Строчка подключения к базе данных (поддерживается только MySql, о формате можно прочитать тут http://docs.sqlalchemy.org/en/latest/dialects/mysql.html)
+DB = 'mysql://be7e037166f6fd:9120268b@eu-cdbr-west-01.cleardb.com/heroku_2628e0f251e73f5?reconnect=true' # Строчка подключения к базе данных (поддерживается только MySql, о формате можно прочитать тут http://docs.sqlalchemy.org/en/latest/dialects/mysql.html)
 API_PORT = 88 # Порт, по которому будет доступен внешний апи бота (REST)
 GOVERNMENT_CHAT =  # Чат, в который бот будет слать отчёты
 SUPER_ADMIN_ID = 272348732 # id супер админа
